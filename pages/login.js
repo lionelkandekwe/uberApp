bc0a3542a18944c1c2ddf00cc6e7497ca05b4a75
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-02-07 22:05:30 +0200

    Design LoginPage

---
+++ pages/login.js
@@ -0,0 +1,31 @@
+import React from "react"
+import tw from "tailwind-styled-components"
+const Login = () => {
+  return (
+    <Wrapper>
+      <UberLogo src="https://i.ibb.co/ZMhy8ws/uber-logo.png" />
+      <Title>Login to access your account</Title>
+      <HeadImage src="https://i.ibb.co/CsV9RYZ/login-image.png" />
+      <SignInButton>Sign in with Google</SignInButton>
+    </Wrapper>
+  )
+}
+
+export default Login
+
+const Wrapper = tw.div`
+flex flex-col h-screen w-screen bg-gray-200 p-4
+`
+const UberLogo = tw.img`
+h-20 w-auto object-contain self-start`
+
+const HeadImage = tw.img`
+object-contain w-full
+`
+
+const Title = tw.div`
+text-5xl pt-4 text-gray-500`
+
+const SignInButton = tw.button`
+bg-black text-white text-center py-4 mt-8 self-center w-full 
+`

e19df4589a98a766707138f91906b435d831b5d3
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-02-07 23:13:44 +0200

    SignIn & Sign Out with Google

--- pages/index.js
+++ pages/index.js
@@ -1,80 +1,101 @@
-import {useEffect} from 'react';
+import { useEffect, useState } from "react"
 import tw from "tailwind-styled-components"
-import Map from './components/Map'
-import Link from 'next/link'
-
+import Map from "./components/Map"
+import Link from "next/link"
+import { auth } from "../firebase"
+import { onAuthStateChanged, signOut } from "firebase/auth"
+import { useRouter } from "next/router"
 export default function Home() {
+  const [user, setUser] = useState(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    return onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser({
+          name: user.displayName,
+          photoUrl: user.photoURL,
+        })
+      } else {
+        setUser(null)
+        router.push("/login")
+      }
+    })
+  }, [])
   return (
     <Wrapper>
-     <Map />
-     <ActionItems>
-     <Header>
-       <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg"/>
-       <Profile>
-         <Name>Lionel Kandekwe</Name>
-         <UserImage src="https://media.istockphoto.com/photos/smiling-indian-business-man-working-on-laptop-at-home-office-young-picture-id1307615661?b=1&k=20&m=1307615661&s=170667a&w=0&h=Zp9_27RVS_UdlIm2k8sa8PuutX9K3HTs8xdK0UfKmYk="/>
-       </Profile>
-     </Header>
-     <ActionButtons>
-       <Link href="/Search">
-     <ActionButton>
-       <ActionButtonImage src='https://i.ibb.co/cyvcpfF/uberx.png'/>
-       Ride
-       </ActionButton>
-       </Link>
-     <ActionButton>
-     <ActionButtonImage src='https://i.ibb.co/n776JLm/bike.png'/>
-       2-Wheels</ActionButton>
-     <ActionButton>
-     <ActionButtonImage src='https://i.ibb.co/5RjchBg/uberschedule.png'/>
-       Reserve</ActionButton>
-     </ActionButtons>
-   
-   <InputButton> Where to go?</InputButton>
+      <Map />
+      <ActionItems>
+        <Header>
+          <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg" />
+          <Profile>
+            <Name>{user && user.name}</Name>
+            <UserImage
+              src={user && user.photoUrl}
+              onClick={() => signOut(auth)}
+            />
+          </Profile>
+        </Header>
+        <ActionButtons>
+          <Link href="/Search">
+            <ActionButton>
+              <ActionButtonImage src="https://i.ibb.co/cyvcpfF/uberx.png" />
+              Ride
+            </ActionButton>
+          </Link>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/n776JLm/bike.png" />
+            2-Wheels
+          </ActionButton>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/5RjchBg/uberschedule.png" />
+            Reserve
+          </ActionButton>
+        </ActionButtons>
 
-     </ActionItems>
+        <InputButton> Where to go?</InputButton>
+      </ActionItems>
     </Wrapper>
   )
 }
 
-
-const Wrapper=tw.div`
+const Wrapper = tw.div`
 flex flex-col h-screen
 `
 
-const ActionItems=tw.div`
+const ActionItems = tw.div`
 flex-1 p-4`
 
-const Header=tw.div`
+const Header = tw.div`
 flex justify-between items-center
 `
 
-const UberLogo=tw.img`h-28`
+const UberLogo = tw.img`h-28`
 
-const Profile=tw.div`
+const Profile = tw.div`
 flex items-center
 `
 
-const Name=tw.div`
+const Name = tw.div`
 mr-4 w-20 text-sm
 `
 
-const UserImage=tw.img`
-h-12 w-12 rounded-full border border-gray-200 p-px
+const UserImage = tw.img`
+h-12 w-12 rounded-full border border-gray-200 p-px cursor-pointer
 `
 
-const ActionButtons=tw.div`
+const ActionButtons = tw.div`
 flex 
 `
 
-const ActionButton=tw.div`
+const ActionButton = tw.div`
 flex bg-gray-200 flex-1 m-1 h-32 items-center justify-center flex-col rounded-lg transform hover:scale-105 transition text-xl
 `
 
-const ActionButtonImage=tw.img`
+const ActionButtonImage = tw.img`
 h-3/5
 `
 
-const InputButton=tw.div`
+const InputButton = tw.div`
 h-20 bg-gray-200 text-2xl p-4 flex items-center mt-8
 `
